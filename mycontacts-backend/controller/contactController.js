6e03984964e2ffc6d053e9fc7c70f7e60da10392
Khannakunal32 <khannakunal32@gmail.com>
2023-08-07 17:32:16 +0530

    added error handling

--- mycontacts-backend/controller/contactController.js
+++ mycontacts-backend/controller/contactController.js
@@ -2,36 +2,47 @@
 //@route GET /api/contacts
 //@access Public
 const getContacts = (req, res) => {
-    res.status(200).json({ messaage: "Get all contacts" });
-}
+  res.status(200).json({ messaage: "Get all contacts" });
+};
 
 //@desc Get contact by id
 //@route GET /api/contacts/:id
 //@access Public
 const getContact = (req, res) => {
-    res.status(200).json({ messaage: `Get contact for ${req.params.id}` });
-}
+  res.status(200).json({ messaage: `Get contact for ${req.params.id}` });
+};
 
 //@desc create contact
 //@route POST /api/contacts
 //@access Public
 const createContact = (req, res) => {
-    console.log(req.body);
-    res.status(201).json({ messaage: `Posted new contact` });
-}
+  console.log("The request body is:", req.body);
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("Please provide name, email and phone");
+  }
+  res.status(201).json({ messaage: `Posted new contact` });
+};
 
 //@desc update contact
 //@route PUT /api/contacts/:id
 //@access Public
 const updateContact = (req, res) => {
-    res.status(201).json({ messaage: `Updated contact for ${req.params.id}` });
-}
+  res.status(201).json({ messaage: `Updated contact for ${req.params.id}` });
+};
 
 //@desc delete contact
 //@route DELETE /api/contacts/:id
 //@access Public
 const deleteContact = (req, res) => {
-    res.status(201).json({ messaage: `Deleted contact for ${req.params.id}` });
-}
+  res.status(201).json({ messaage: `Deleted contact for ${req.params.id}` });
+};
 
-module.exports = { getContacts, getContact, createContact, updateContact, deleteContact };
+module.exports = {
+  getContacts,
+  getContact,
+  createContact,
+  updateContact,
+  deleteContact,
+};
--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+// middleware to pass json request body
+app.use(express.json());
+
 
 // 1 method directly in server.js
 app.get("/", (req, res) => {
@@ -15,6 +20,9 @@ app.use("/api/second", require("./routes/secondExampleRoutes"));
 
 // 3 method using routes with controller
 app.use("/api/contacts", require("./routes/contactRoutes"));
+
+// error handler middleware
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
