dde76c8d446f77726658c3d8b780855fe42930be
Khannakunal32 <khannakunal32@gmail.com>
2023-08-10 14:39:57 +0530

    2nd way of using styled componenet with dynamic

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -11,15 +11,17 @@ const FormControl = styled.div`
     font-weight: bold;
     display: block;
     margin-bottom: 0.5rem;
+    color: ${(props) => (props.invalid ? '#dc3545' : 'black')};
   }
 
   & input {
     display: block;
     width: 100%;
-    border: 1px solid #ccc;
+    border: 1px solid ${(props) => (props.invalid ? '#dc3545' : '#ccc')};
     font: inherit;
     line-height: 1.5rem;
     padding: 0 0.25rem;
+    background: ${(props) => (props.invalid ? '#ffd7d9' : 'transparent')};
   }
 
   & input:focus {
@@ -61,8 +63,8 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl className={`${!isValid && 'invalid'}`}>
-        <label>Course Goal</label>
+      <FormControl invalid={!isValid}>
+        <label >Course Goal</label>
         <input
           type="text"
           onChange={goalInputChangeHandler}
